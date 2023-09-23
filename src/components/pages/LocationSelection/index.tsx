1143dde043cb81c6f9acb740b8d17a359c5c2cd8
star <chlvkfksquf@gmail.com>
2023-09-24 01:36:42 +0900

    Modify layout style

--- src/components/pages/LocationSelection/index.tsx
+++ src/components/pages/LocationSelection/index.tsx
@@ -12,7 +12,8 @@ const LocationSelection = () => {
   const [latLong, setLatLong] = useRecoilState(latLongState)
 
   return (
-    <div className='w-full relative'>
+    <div className='w-full relative px-6'>
+      <div className='w-full h-10' />
       <div className=' text-xl font-semibold'>
         물건을 전달할 장소를 선택해주세요.
       </div>
@@ -24,7 +25,7 @@ const LocationSelection = () => {
       <KakaoMap setLocationName={setLocationName} setLatLong={setLatLong} />
       {locationName && (
         <Link to='/create'>
-          <button className='absolute bottom-[50px] bg-red-400 w-full z-10 py-4 text-center rounded-[10px] text-white font-semibold text-base'>
+          <button className='absolute bottom-[50px] bg-red-400 w-[calc(100%-48px)] z-10 py-4 text-center rounded-[10px] text-white font-semibold text-base'>
             선택 완료
           </button>
         </Link>
