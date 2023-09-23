df97d9bf0b404901b217d8cd95e8c840fbdf4966
star <chlvkfksquf@gmail.com>
2023-09-23 19:23:47 +0900

    Add: 타이머 게임

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,11 +3,13 @@ import { Route, Routes as ReactRoutes } from 'react-router-dom'
 import MainPage from 'pages/MainPage'
 import CreatePage from 'pages/CreatePage'
 import LocationSelectionPage from 'pages/LocationSelectionPage'
+import TimeGamePage from 'pages/TimeGamePage'
 
 const Routes = () => {
   return (
     <ReactRoutes>
       <Route path='/' element={<MainPage />} />
+      <Route path='/games/time' element={<TimeGamePage />} />
       <Route path='/create' element={<CreatePage />} />
       <Route
         path='/create/location-selection'
