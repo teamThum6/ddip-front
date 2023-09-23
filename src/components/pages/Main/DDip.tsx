5b77bbcecd85da3588f9400580c59abb21782735
star <chlvkfksquf@gmail.com>
2023-09-24 07:55:08 +0900

    Add: 라우팅 연결

--- src/components/pages/Main/DDip.tsx
+++ src/components/pages/Main/DDip.tsx
@@ -1,9 +1,18 @@
+import { useNavigate } from 'react-router-dom'
+
 import Spacing from 'layouts/Spacing'
 import style from './DDip.module.css'
 
 function DDip() {
+  const navigate = useNavigate()
+
   return (
-    <button className={style.circle}>
+    <button
+      className={style.circle}
+      onClick={() => {
+        navigate('/create')
+      }}
+    >
       <img src='/assets/ddip-cta.png' className='w-[31px]' alt='' />
       <Spacing size={2} />
       <p className='text-white text-[10px] leading-3'>띱 하기</p>
